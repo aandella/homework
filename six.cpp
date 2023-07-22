6f2181f76b317811c237ecff2122c5eca1871aea
andella <lanadelrayy@gmail.com>
2023-07-22 15:45:39 +0300

    6 задание

---
+++ six.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+string secWord(string a)
+{
+    string word;
+    string res;
+    const int SIZE = a.length();
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (a[i] != ' ')
+        {
+            word = word + a[i];
+        }
+        else
+        {
+            for (int j = i + 1; j < SIZE; j++)
+            {
+                res = res + a[j];
+            }
+            break;
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    cout << "Enter a sentense: ";
+    string sent;
+    getline(cin, sent);
+    string res = secWord(sent);
+    cout << res << endl;
+}
